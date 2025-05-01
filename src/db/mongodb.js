aa27c6a00e07db45fd2802cf275101a799525c42
Adarsh Singh <94136105+AdarshsinghDev@users.noreply.github.com>
2025-05-01 21:51:20 +0530

    Update mongodb.js

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/Adarsh").then(() => {
+mongoose.connect("mongodb+srv://adarsh:adarsh123@cluster0.psvz3uu.mongodb.net/Adarsh").then(() => {
     console.log(`✅ MongoDB connected successful!`)
 }).catch((error) => {
     console.log(`Db error: ${error}`);
